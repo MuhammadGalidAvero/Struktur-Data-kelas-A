ef851609f1f0be6c4439c2bd0a1a2ecc0ad7a22b
Cyborg 15 <mgalidavero05@gmail.com>
2024-05-21 11:51:42 +0700

    Commit praktikum 21 Mei pekan 5 (mati lampu)

---
+++ src/Pekan_5/InsertionSort.java
@@ -0,0 +1,33 @@
+package Pekan_5;
+
+public class InsertionSort {
+	void sort (int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+			
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+	
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+	}
+	
+	public static void main (String [] args) {
+		int arr[] = {3, 10, 4, 6, 8, 9, 7, 2, 1, 5};
+		
+		InsertionSort ob = new InsertionSort();
+		ob.sort(arr);
+		
+		printArray(arr);
+	}
+}
