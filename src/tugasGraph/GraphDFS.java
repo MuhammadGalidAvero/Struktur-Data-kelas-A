7c9b7edff40dc5342e3d4c7ee7aede6a4602827a
Cyborg 15 <mgalidavero05@gmail.com>
2024-06-12 21:33:07 +0700

    Commit Tugas Struktur Data 12 Juni 2024, graph BFS dan DFS

---
+++ src/tugasGraph/GraphDFS.java
@@ -0,0 +1,52 @@
+package tugasGraph;
+import java.util.*;
+
+public class GraphDFS {
+    private int numVertices;
+    private List<List<Integer>> adjacencyList;
+
+    public GraphDFS(int numVertices) {
+        this.numVertices = numVertices;
+        adjacencyList = new ArrayList<>(numVertices);
+        for (int i = 0; i < numVertices; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int src, int dest) {
+        adjacencyList.get(src).add(dest);
+        adjacencyList.get(dest).add(src); // Untuk graf tidak berarah
+    }
+
+    public void executeDFS(int startVertex) {
+        boolean[] visited = new boolean[numVertices];
+        DFSUtil(startVertex, visited);
+    }
+
+    private void DFSUtil(int vertex, boolean[] visited) {
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+
+        for (int neighbor : adjacencyList.get(vertex)) {
+            if (!visited[neighbor]) {
+                DFSUtil(neighbor, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphDFS graph = new GraphDFS(8);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+        graph.addEdge(5, 7);
+        graph.addEdge(6, 7);
+
+        System.out.println("Melakukan DFS traversal dari vertex 0:");
+        graph.executeDFS(0);
+    }
+}
